aa0bde7fd0787fecea495d865a7a0698c400bcac
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2023-01-18 08:04:27 +0500

    Update binance.py

--- binance.py
+++ binance.py
@@ -3,17 +3,11 @@ import json
 import ccxt
 from ccxt.base.errors import InvalidAddress, InvalidOrder, ExchangeError
 from termcolor import cprint
-from web3 import Web3
 import random
 from config import *
 
-def binance_withdraw(privatekey, amount_to_withdraw, symbol_to_withdraw, network):
+def binance_withdraw(address_wallet, amount_to_withdraw, symbol_to_withdraw, network):
 
-    if len(privatekey) > 50:
-        web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
-        address_wallet = web3.eth.account.privateKeyToAccount(privatekey).address
-    else:
-        address_wallet = privatekey
 
     account_binance = ccxt.binance({
         'apiKey': BINANCE_API_KEY,
